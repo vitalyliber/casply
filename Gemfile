299f507c4666f4e3170e3632c68e8a766220c527
vitalyliber <zenamax@gmail.com>
2018-03-11 18:29:13 +0700

    added locales and home controller

--- Gemfile
+++ Gemfile
@@ -65,3 +65,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Devise
 gem 'devise'
+
+# Slim
+gem 'slim-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    browser_locale = extract_locale_from_accept_language_header
+    I18n.locale = browser_locale.in?(['ru', 'en']) ? browser_locale : 'en'
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
 end
