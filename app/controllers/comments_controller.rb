987d4ddbe8765492cc05427ce1524437563c89ae
vitalyliber <zenamax@gmail.com>
2018-03-17 21:17:46 +0700

    added method create for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+  def create
+    comment = current_user.comments.build(comment_params)
+    comment.costume_id = params[:costume_id]
+    comment.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
