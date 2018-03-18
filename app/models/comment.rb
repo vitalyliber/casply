ed358510035517ff68d2462b8bc5c2d33b23e5f7
vitalyliber <zenamax@gmail.com>
2018-03-18 16:59:47 +0700

    number of little improvements

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   validates_presence_of :text, :user
   validates_length_of :text, minimum: 3, maximum: 455
-  belongs_to :costume, counter_cache: true, dependent: :destroy
+  belongs_to :costume, counter_cache: true
   belongs_to :user
 end
