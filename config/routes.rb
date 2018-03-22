5a5ef70b7cfb3253787f7d296f429aed526fb6f4
vitalyliber <zenamax@gmail.com>
2018-03-22 21:59:00 +0700

    change the root controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "costumes#index"
 end
