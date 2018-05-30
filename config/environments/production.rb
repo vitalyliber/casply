d32323ee6468e56e94f9d487154533324ce8f240
vitalyliber <zenamax@gmail.com>
2018-05-30 15:59:04 +0700

    change email sender domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
       :address        => ENV['MAILGUN_SMTP_SERVER'],
       :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
       :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'casply.heroku.com',
+      :domain         => 'casply.com',
       :authentication => :plain,
   }
     config.action_mailer.delivery_method = :smtp
