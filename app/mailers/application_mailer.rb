613a65cfe0a35513659a566274d3c0af56d57d6d
vitalyliber <zenamax@gmail.com>
2018-06-08 02:06:27 +0700

    title of mailer was changed

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'Casply <noreply@casply.com>'
   layout 'mailer'
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'noreply@casply.com'
+  config.mailer_sender = 'Casply <noreply@casply.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
